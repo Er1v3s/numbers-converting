c665f8ca38880b62441620b0b775770d2621a32c
Filip Statkiewicz <stf6.dev@gmail.com>
2023-05-09 14:29:05 +0200

    Using backspace funcionality, input char validator

--- numbers-converting.asm
+++ numbers-converting.asm
@@ -10,18 +10,22 @@ start:
 
 program:
 	wyswietl	msg1
-	mov	ecx,16
+	mov	ecx,5	       ; inicjalizacja licznika pêtli
 input:
-	pob_znak_ECHO
+	pob_znak
 
 	cmp	al,51h		; ASCII Q key
 	je	end_program
-	cmp	al,71h
-	je	end_program	; ASCII q key
-
+	cmp	al,71h		; ASCII q key
+	je	end_program
 	cmp	al,08h		; ASCII backspace key
-	je	clear_input
+	je	remove_char
+	jmp	char_validator
 
+   correct_validation:
+
+	wysw_znak	al
+	inc	[counter]
 	loop	input
 
 	add	     [row_position],2
@@ -42,23 +46,55 @@ input:
 	wyswietl     result_hex
 
 	ustaw_kursor [row_position],0
+	mov	     [counter], 15
 	call	     program
 
+char_validator:
+	cmp	al, 30h
+	je	correct_validation
+	cmp	al, 31h
+	je	correct_validation
+	cmp	al, 32h
+	je	correct_validation
+	cmp	al, 33h
+	je	correct_validation
+	cmp	al, 34h
+	je	correct_validation
+	cmp	al, 35h
+	je	correct_validation
+	cmp	al, 36h
+	je	correct_validation
+	cmp	al, 37h
+	je	correct_validation
+	cmp	al, 38h
+	je	correct_validation
+	cmp	al, 39h
+	je	correct_validation
+   invalid_input:
+	jmp	input
 
 end_program:
 	invoke ExitProcess
 
-clear_input:
-	wyswietl       test_msg
+remove_char:
+	cmp	[counter],15	  ; 15 is minimal coutner value
+	jbe	continue
+	dec	[counter]
+	inc	ecx		  ;
+   continue:
+	ustaw_kursor [row_position],[counter]
+	wysw_znak	20h
+	ustaw_kursor [row_position],[counter]
 	jmp    input
 
 section '.data' data readable writeable
 
 	row_position	dw	0,0
+	counter 	dw	15,0	; msg1 length
 
-	test_msg	db	'Dzialam!',0
 	msg1	db	'Wprowadz dane: ',0
 	result_bin	db	  '[bin] = Wynik w postaci binarnej',0
 	result_oct	db	  '[oct] = Wynik w postaci oktalnej',0
 	result_dec	db	  '[dec] = Wynik w postaci decymalnej',0
 	result_hex	db	  '[hex] = Wynik w postaci hexadecymalnej',0
+
