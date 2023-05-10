95bee748592f9964a484b43572017e82e09d1eef
Filip Statkiewicz <stf6.dev@gmail.com>
2023-05-10 20:23:23 +0200

    reafctor: Clean the code

--- numbers-converting.asm
+++ numbers-converting.asm
@@ -9,9 +9,9 @@ section '.code' code readable executable
 start:
 	wyswietl	msg1
 	mov	ecx,16		; loop counter initialization
-	xor	ebx, ebx       ; clear registers before the next loop run
-	xor	eax, eax       ; clear registers before the next loop run
-input:
+	xor	ebx,ebx 	; clear registers before the next loop run
+	xor	eax,eax 	; clear registers before the next loop run
+   input:
 	pob_znak
 
 	cmp	al,51h		; ASCII Q key
@@ -28,7 +28,7 @@ input:
 	wysw_znak	al
 	inc	[counter]
 
-	sub	al, '0' 	; from hex to ASCII
+	sub	al,'0'		; from hex to ASCII
 	or	ebx,ebx 	; check ebx register is empty
 	jz	first_bit	; if yes create first bit
 	rcl	ebx,1		; shift bits to left
@@ -38,7 +38,7 @@ input:
    return_after_bits_operation:
 	loop	input
 
-leave_loop:
+   leave_loop:
 	mov	[temp_ebx], bx
 
 ;SHOW_BIN
@@ -47,19 +47,18 @@ leave_loop:
 	inc	     [row_position]
 	wyswietl     result_bin_text
 
-	mov	     [result_bin], bx
-	mov cx, 16
-	mov bx, [result_bin]
+	mov	     cx,16
+	mov	     bx,[temp_ebx]
 
    bin_ety1:
 	push	cx
-	rcl	bx, 1
+	rcl	bx,1
 	jc	bin_ety2
-	mov	dl, '0'
+	mov	dl,'0'
 	jmp	bin_ety3
 
    bin_ety2:
-	mov	dl, '1'
+	mov	dl,'1'
 
    bin_ety3:
 	wysw_znak	dl
@@ -67,60 +66,54 @@ leave_loop:
 	pop	cx
 	loop	bin_ety1
 
-;SHOW_HEX
+;SHOW HEX
 	ustaw_kursor [row_position],0
 	inc	     [row_position]
 	wyswietl     result_hex_text
 
-;SHOW_OCT
+;SHOW OCT
 	ustaw_kursor [row_position],0
 	inc	     [row_position]
 	wyswietl     result_oct_text
 
-;SHOW_DEC
+;SHOW DEC
 	ustaw_kursor [row_position],0
 	add	     [row_position],2
 	wyswietl     result_dec_text
 
-	mov	ax,[temp_ebx]  ; wczytanie wartoœci 16-bitowej do rejestru AX
+	mov	ax,[temp_ebx]
+	mov	bx,10
+	mov	cx,0
 
-	mov	bx, 10		; inicjalizacja rejestru BX wartoœci¹ 10
-	mov	cx, 0		; inicjalizacja rejestru CX zerem
    dec_ety1:
-	xor	dx, dx		; inicjalizacja rejestru DX zerem
-	div	bx		; podzielenie wartoœci w rejestrze AX przez 10
-	push	dx		; zapisanie wyniku na stosie
-	inc	cx		; inkrementacja licznika cyfr dziesiêtnych
-	test	ax, ax		; sprawdzenie, czy wartoœæ w rejestrze AX wynosi 0
-	jnz	dec_ety1	; jeœli nie wynosi, to powrót do konwersji
+	xor	dx,dx
+	div	bx		; division ax by bx
+	push	dx
+	inc	cx
+	test	ax, ax		; ax == 0 ?
+	jnz	dec_ety1
 
-; wyœwietlenie cyfr dziesiêtnych
    dec_ety2:
-	pop	dx		; pobranie wyniku z stosu
-	add	dl, '0' 	; dodanie wartoœci '0' (w ASCII) do rejestru DL
-	wysw_znak   dl		; wywo³anie makra wysw_znak
-	loop	    dec_ety2	; powtórzenie wyœwietlenia dla pozosta³ych cyfr
+	pop	dx
+	add	dl, '0' 	; from hex to ASCII
+	wysw_znak   dl
+	loop	dec_ety2
 
-
-
-
-
-
+;RESTART PROGRAM
 	ustaw_kursor [row_position],0
-	mov	     [counter], 15
-
+	mov	     [counter],15
 	call	     start
 
 char_validator:
-	cmp	al, 30h
+	cmp	al,30h
 	je	correct_validation
-	cmp	al, 31h
+	cmp	al,31h
 	je	correct_validation
    invalid_input:
 	jmp	input
 
 first_bit:
-	mov	ebx, eax
+	mov	ebx,eax
 	jmp	return_after_bits_operation
 
 add_one:
@@ -150,11 +143,7 @@ section '.data' data readable writeable
 
 	msg1	db	'Wprowadz dane: ',0
 	result_bin_text      db        '[bin] = ',0
-	result_bin	     dw        ?,0
 	result_hex_text      db        '[hex] = ',0
-	result_hex	     dd        "d",0
 	result_oct_text      db        '[oct] = ',0
-	result_oct	     dw        ?,0
 	result_dec_text      db        '[dec] = ',0
-	result_dec	     dw        ?,0
 
